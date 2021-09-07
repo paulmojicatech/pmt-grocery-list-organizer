e444c5b814d1fa5739a66335fd2e65b7f074ff68
Paul Mojica <paulmojicatech@gmail.com>
2021-09-06 20:52:04 -0400

    add plugin

--- apps/grocery-ionic/src/app/app.component.ts
+++ apps/grocery-ionic/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from'@capacitor/core';
 
 @Component({
   selector: 'paulmojicatech-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+    const { SecureConfigStorage } = Plugins;
+    SecureConfigStorage.getValueFromConfig('testing').then((value: { result: string }) => {
+      console.log(value.result);
+    })
+  }
 }
