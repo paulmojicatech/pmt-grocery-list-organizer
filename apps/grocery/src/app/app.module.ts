ebe6577395fc00eea7ec7ab1c51811f9f6cae3c8
Paul Mojica <paulmojicatech@gmail.com>
2021-09-17 17:51:38 -0400

    add angular material to web

--- apps/grocery/src/app/app.module.ts
+++ apps/grocery/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { RootStateModule } from '../../../../libs/grocery-shared-business-logic/src/lib/state/state.module';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, RootStateModule],
+  imports: [BrowserModule, AppRoutingModule, RootStateModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
