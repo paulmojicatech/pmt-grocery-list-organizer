365bcba7266c7fd7d0f0c3e694e8ab1580dd83d6
Paul Mojica <paulmojicatech@gmail.com>
2021-09-18 06:12:33 -0400

    add material icons add material toolbar to home

--- apps/grocery/src/app/home/home.module.ts
+++ apps/grocery/src/app/home/home.module.ts
@@ -1,11 +1,14 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { HomeRoutingModule } from "./home-routing.module";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        HomeRoutingModule
-    ]
+  imports: [CommonModule, MatMenuModule, MatToolbarModule, HomeRoutingModule],
+  declarations: [HomeComponent]
 })
 export class HomeModule {}
