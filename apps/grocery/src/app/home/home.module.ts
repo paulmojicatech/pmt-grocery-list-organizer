d9d69939966ae20b7a24583b040232b79c5831f2
Paul Mojica <paulmojicatech@gmail.com>
2021-10-09 13:02:14 -0400

    finish ui for side panel

--- apps/grocery/src/app/home/home.module.ts
+++ apps/grocery/src/app/home/home.module.ts
@@ -8,6 +8,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -22,6 +24,8 @@ import { HomeComponent } from './home.component';
     MatFormFieldModule,
     MatSelectModule,
     MatAutocompleteModule,
+    MatButtonModule,
+    MatSlideToggleModule,
     HomeRoutingModule,
   ],
   declarations: [HomeComponent],
