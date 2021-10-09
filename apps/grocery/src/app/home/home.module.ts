0611b6a23f3798e8ed20c38c8cd145a3f3e8603c
Paul Mojica <paulmojicatech@gmail.com>
2021-10-09 02:10:41 -0400

    styling changes

--- apps/grocery/src/app/home/home.module.ts
+++ apps/grocery/src/app/home/home.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -14,6 +16,8 @@ import { HomeComponent } from './home.component';
     MatMenuModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatInputModule,
+    MatFormFieldModule,
     HomeRoutingModule,
   ],
   declarations: [HomeComponent],
