15bd7d72ef7863ef385bd76fba385aae2246a1b5
Paul Mojica <paulmojicatech@gmail.com>
2021-10-09 01:43:42 -0400

    add side panel for web

--- apps/grocery/src/app/home/home.module.ts
+++ apps/grocery/src/app/home/home.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  imports: [CommonModule, MatMenuModule, MatToolbarModule, HomeRoutingModule],
-  declarations: [HomeComponent]
+  imports: [
+    CommonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    HomeRoutingModule,
+  ],
+  declarations: [HomeComponent],
 })
 export class HomeModule {}
