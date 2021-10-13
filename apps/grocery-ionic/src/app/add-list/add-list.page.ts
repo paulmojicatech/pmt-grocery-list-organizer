b39eca22257e969399ec7287db74e0a0cc046b7a
Paul Mojica <paulmojicatech@gmail.com>
2021-10-13 05:44:03 -0400

    add action bar

--- apps/grocery-ionic/src/app/add-list/add-list.page.ts
+++ apps/grocery-ionic/src/app/add-list/add-list.page.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
-    templateUrl: './add-list.page.html'
+    templateUrl: './add-list.page.html',
+    styleUrls: ['./add-list.page.scss']
 })
 export class IonicAddListComponent {
     
