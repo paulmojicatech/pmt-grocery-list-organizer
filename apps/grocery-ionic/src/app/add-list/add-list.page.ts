1404d6f4de6762115b77541f3963aef89592edcf
Paul Mojica <paulmojicatech@gmail.com>
2021-11-07 15:46:15 -0500

    add item form for ionic

--- apps/grocery-ionic/src/app/add-list/add-list.page.ts
+++ apps/grocery-ionic/src/app/add-list/add-list.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
+import { AppViewModel } from "libs/grocery-shared-business-logic/src/lib/app-view-model/app-state.interface";
+import { IonicAppStateService } from "libs/grocery-shared-business-logic/src/lib/app-view-model/ionic/ionic-app-state.service";
+import { Observable } from "rxjs";
 
 import { AddItemFormService } from '../../../../../libs/grocery-shared-business-logic/src/lib/items/add-item-form.service';
 
@@ -9,11 +12,13 @@ import { AddItemFormService } from '../../../../../libs/grocery-shared-business-
 })
 export class IonicAddListComponent implements OnInit {
     addItemForm!: FormGroup;
+    viewModel$!: Observable<AppViewModel>;
 
-    constructor(private _formService: AddItemFormService){}
+    constructor(private _formService: AddItemFormService, private _appViewModelSvc: IonicAppStateService){}
 
 
     ngOnInit(): void {
         this.addItemForm = this._formService.getAddItemFormGroup();
+        this.viewModel$ = this._appViewModelSvc.getViewModel();
     }
 }
--- apps/grocery-ionic/src/app/app.module.ts
+++ apps/grocery-ionic/src/app/app.module.ts
@@ -11,11 +11,12 @@ import { AppReducer } from '../../../../libs/grocery-shared-business-logic/src/l
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), StoreModule.forRoot({}), StoreModule.forFeature('app', AppReducer), EffectsModule.forRoot([]), StoreDevtoolsModule.instrument({}), AppRoutingModule],
+  imports: [BrowserModule, ReactiveFormsModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), StoreModule.forRoot({}), StoreModule.forFeature('app', AppReducer), EffectsModule.forRoot([]), StoreDevtoolsModule.instrument({}), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
